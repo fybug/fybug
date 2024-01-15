9427b06abae166ec467c16af3def25a606e7374f
fybug <1006291762@qq.com>
2024-01-15 14:57:40 +0800

    合并 aout.ass v0.0.1-fix-7 补丁，更新简历

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,10 +3,5 @@ const config = require('./webpack.conf.js');
 
 module.exports = merge(config, {
     mode: "none",
-    // 代码压缩插件
-    optimization: {minimize: true},
-    devtool: 'null',
-    plugins: [
-        new webpack.DefinePlugin({'process.env.NODE_ENV': JSON.stringify('production')})
-    ]
+    devtool: 'null'
 });
